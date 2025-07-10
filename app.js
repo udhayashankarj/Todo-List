ed53af7a68e92959cad5e2d908f8d642d7451549
shank <220701306@rajalakshmi.edu.in>
2025-07-10 23:56:12 +0530

    Some changes for deployment

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("public", path.join(__dirname, "./public"));
 app.set("views", path.join(__dirname, "./views"));
 
 app.use(compression());
